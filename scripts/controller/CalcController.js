2d42faf80639dfdede12fb0ab0fa765ae3c7de00
Mauricio <mjunior447@gmail.com>
2021-01-20 10:34:05 -0300

    criadas funcionalidades de mostrar data e hora atuais, atualizando as informações no display a cada segundo

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,64 @@
+class CalcController {
+
+    constructor() {
+        this._displayCalcEl = document.querySelector('#display');
+        this._timeEl = document.querySelector('#hora');
+        this._dateEl = document.querySelector('#data');
+        this._locale = 'pt-br';
+        //this._currentDate;
+        this.initialize();
+    }
+    
+    initialize() {
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
+
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date();
+    }
+
+    set currentDate(value) {
+        this._currentDate = value;
+    }
+
+    
+
+}
