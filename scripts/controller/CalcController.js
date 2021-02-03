619cde9eb5ded285656b9854aad27953ccd09998
Mauricio <mjunior447@gmail.com>
2021-02-02 21:47:20 -0300

    desenvolvidas as funcionalidades de todas as operações básicas (adição, subtração, multiplicação, divisão e porcentagem; desenvolvida funcionalidade do botão '='; criada função para atualizar o display sempre que um novo número ou operador forem inseridos

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -18,42 +18,146 @@ class CalcController {
             this.setDisplayDateTime();
         }, 1000);
 
+        this.setLastNumerToDisplay();
+
     }
     
     // apaga tudo que está no console da calculadora, inclusive historico
     allClear() {
         this._operation = [];
+
+        this.setLastNumerToDisplay();
     }
 
     // apaga a última entrada no console da calculadora, preservando historico
     clearEntry() {
         this._operation.pop();
+
+        this.setLastNumerToDisplay();
     }
-
-    porcento() {}
-
-    divisao() {}
-
-    multiplicacao() {}
-
-    subtracao() {}
-
-    soma() {}
-
-    igual() {}
 
     setError() {
         this.displayCalc = 'Error';
     }
 
-    addOperation(op) {
-        this._operation.push(op);
-        console.log (this._operation);
+    getLastOperator() {
+        let lastOperator;
+        for (let i = 0; i < this._operation.length; i++) {
+            if (isNaN(this._operation[i])) {
+                lastOperator = this._operation[i];
+                break;
+            }
+        }
+        return lastOperator;
     }
 
-    execBtn(value) {
+    getLastElement() {
+        return this._operation[this._operation.length - 1];
+    }
 
-        switch(value) {
+    setLastElement(value) {
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value) {
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+    }
+
+    concatExpression() {
+        let expression = this._operation.join('');
+        return expression;
+    }
+
+    calculate() {
+        let expression = this.concatExpression();
+        let result = eval(expression);
+        return result;
+    }
+
+    equals() {
+        // só é possivel usar o 'igual' se houver
+        // pelo menos dois operandos e um operador no array
+        if (this._operation.length > 2) {
+            this._operation = [this.calculate()];
+            this.setLastNumerToDisplay();
+        }
+    }
+
+    // insertDot() {
+    //     // caso o ponto não exista no último número
+    //     if (!(this.getLastElement().toString().indexOf('.') >= -1)) {
+    //         let newOperator = this._operation.pop().toString() + '.';
+    //         this._operation.push(newOperator);
+    //         this.setLastNumerToDisplay();
+    //     } else {
+    //         console.log('aqui')
+    //     }
+    // }
+
+    pushOperation(value) {
+        if (this._operation.length > 2) {
+            this._operation = [this.calculate()];
+        }
+
+        if (value == '%') {
+            let result = this.getLastElement();
+            result /= 100;
+            this._operation = [result];
+        } else {
+            this._operation.push(value);
+        }
+
+        this.setLastNumerToDisplay();
+    }
+
+    // atualiza o display
+    setLastNumerToDisplay() {
+        let lastNumber;
+
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+            if (!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+        if (!lastNumber) lastNumber = 0;
+
+        this.displayCalc = lastNumber;
+    }
+
+    // insere o último operador ou número na pilha, tratando cada caso
+    addOperation(op) {
+        if (isNaN(this.getLastElement())) {
+            if (this.isOperator(op)) {
+                this.setLastElement(op);
+            } else {
+                this.pushOperation(op);
+                this.setLastNumerToDisplay();
+            }
+
+        } else {
+            if (this.isOperator(op)) {
+                this.pushOperation(op);
+            } else {
+                let newOp = this.getLastElement().toString() + op.toString();
+                this.setLastElement(parseFloat(newOp));
+                this.setLastNumerToDisplay();
+            }
+
+        }
+    }
+
+    addDot() {
+        let lastOperator = this.getLastOperator();
+        console.log(lastOperator);
+    }
+
+    // adiciona a funcionalidade de cada botão
+    // o botão "AC" significa "All Clear (limpar tudo)"
+    // o botão "CE" significa "Clear Entry (limpar última entrada)"
+    execBtn(textBtn) {
+
+        switch(textBtn) {
             case 'ac':
                 this.allClear();
                 break;
@@ -63,27 +167,31 @@ class CalcController {
                 break;
 
             case 'porcento':
-                this.porcento();
+                this.addOperation('%');
                 break;
 
             case 'divisao':
-                this.divisao();
+                this.addOperation('/');
                 break;
 
             case 'multiplicacao':
-                this.multiplicacao();
+                this.addOperation('*');
                 break;
 
             case 'subtracao':
-                this.subtracao();
+                this.addOperation('-');
                 break;
 
             case 'soma':
-                this.soma();
+                this.addOperation('+');
                 break;
 
             case 'igual':
-                this.igual();
+                this.equals();
+                break;
+
+            case 'ponto':
+                this.addDot();
                 break;
 
             case '0':
@@ -96,7 +204,7 @@ class CalcController {
             case '7':
             case '8':
             case '9':
-                this.addOperation(parseInt(value));
+                this.addOperation(parseInt(textBtn));
                 break;
 
             default:
@@ -104,19 +212,14 @@ class CalcController {
                 break;
             
         }
-
     }
 
     initButtonEvents() {
-
-        // o botão "AC" significa "All Clear (limpar tudo)"
-        // o botão "CE" significa "Clear Entry (limpa última entrada)"
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach(btn => {
             this.addEventListenerAll(btn, 'click drag', e => {
                 let textBtn = btn.className.baseVal.replace("btn-", "");
-                console.log(textBtn);
                 this.execBtn(textBtn);
             });
 
