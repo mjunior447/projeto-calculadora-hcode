36375fda5f08d5887d26a11ea797d8fdac0389ba
Mauricio <mjunior447@gmail.com>
2021-02-03 15:18:48 -0300

    criados eventos de teclado para suportar números e operações digitados; implementada funcionalidade de copiar/colar um número do/no display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._locale = 'pt-br';
         this.initialize();
         this.initButtonEvents();
+        this.initKeyboard();
     }
     
     initialize() {
@@ -18,7 +19,27 @@ class CalcController {
         }, 1000);
 
         this.setLastNumerToDisplay();
+        
+        this.pasteFromClipboard();
 
+    }
+
+    // função para copiar texto do display
+    copyToClipboard() {
+        let input = document.createElement('input');
+        input.value = this.displayCalc;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('Copy');
+        input.remove();
+    }
+
+    // função para colar o texto da área de transferência
+    pasteFromClipboard() {
+        document.addEventListener('paste', e => {
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        })
     }
     
     // apaga tudo que está no console da calculadora, inclusive historico
@@ -233,7 +254,59 @@ class CalcController {
                 btn.style.cursor = 'pointer';
             })
         });
+    }
 
+    initKeyboard() {
+        document.addEventListener('keyup', e => {
+            console.log(e.key);
+
+            switch(e.key) {
+                case 'Escape':
+                    this.allClear();
+                    break;
+
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+
+                case 'Enter':
+                case '=':
+                    this.equals();
+                    break;
+
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(e.key);
+                    break;
+
+                // caso o usuário pressione ctrl+c
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
+                    break;
+
+            }
+        });
     }
 
     addEventListenerAll(element, events, fn) {
