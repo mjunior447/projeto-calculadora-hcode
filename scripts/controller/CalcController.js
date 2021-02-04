384d1607a14f34370f1d7308bba7fd935803724d
Mauricio <mjunior447@gmail.com>
2021-02-03 22:16:17 -0300

    definido o máximo de dígitos que podem aparecer no visor da calculadora. Caso ultrapasse isso, ela retornará um ERROR

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -367,6 +367,11 @@ class CalcController {
     }
 
     set displayCalc(value) {
+        if (value.toString().length > 10) {
+            this.setError();
+            return;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
